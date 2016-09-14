c851f2e6ac53673e72bf96b83260a4718b1952af
Ubuntu <ubuntu@ip-172-31-15-89.us-west-1.compute.internal>
2016-09-14 06:36:38 +0000

    Creating databse in init

--- reshare/__init__.py
+++ reshare/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from instance.items_config
+from instance.db_config import items_config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(items_config['user'], items_config['password'], items_config['host'], items_config['port'], items_config['db_name'])
+db = SQLAlchemy(app)

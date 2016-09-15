b3f297dbf97c3ba9cef6d9feb700d3675fad6625
Ubuntu <ubuntu@ip-172-31-15-89.us-west-1.compute.internal>
2016-09-15 03:19:48 +0000

    File to start application and create server objects

--- reshare/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from instance.db_config import items_config
-
-# Should these move to another module and then I can import them elsewhere?
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(items_config['user'], items_config['password'], items_config['host'], items_config['port'], items_config['db_name'])
-db = SQLAlchemy(app)

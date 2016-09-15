af7f91dc9ec9aee3424fd019d29b8f800c494734
Ubuntu <ubuntu@ip-172-31-15-89.us-west-1.compute.internal>
2016-09-15 06:35:58 +0000

    Making changes to database tables and preparing first data model

--- reshare/application.py
+++ reshare/application.py
@@ -2,5 +2,7 @@ from flask import Flask
 from instance.db_config import items_config
 
 app = Flask(__name__)
+
+# This might be postgresql+psycopg2
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(items_config['user'], items_config['password'], items_config['host'], items_config['port'], items_config['db_name']
 db = SQLAlchemy(app)
--- reshare/models/listings.py
+++ reshare/models/listings.py
@@ -2,6 +2,28 @@ from application import app, db
 
 
 class Listing(db.Model):
-	listing_id = db.Column(db.UUID, primary_key=True)
+	listing_id = db.Column('listing_id', db.UUID(), primary_key=True)
+	price_per_hour = db.Column('price_per_hour', db.REAL())
+	price_per_day = db.Column('price_per_day', db.REAL())
+	price_per_week = db.Column('price_per_week', db.REAL(), default=-1)
+	maximum_time = db.Column('maximum_time', db.INTEGER)
+	minimum_time = db.Column('minimum_time', db.INTEGER)
 	
+	has_delivery = db.Column('has_delivery', db.BOOLEAN(), default=False)
+	delivery_price = db.Column('delivery_price', db.REAL(), default=0)
+	
+	late_fee = db.Column('late_fee', db.REAL())
+	broken_price = db.Column('broken_price', db.REAL())
+
+	title = db.Column('title', db.VARCHAR(512), nullable=False)
+	description = db.Column('description', db.TEXT())
+	
+	is_closed = db.Column('is_closed', db.BOOLEAN(), default=False)
+	creation_timestamp = db.Column('creation_timestamp', timezone=True, nullable=False)
+
+	def __init__(self, params):
+
+	def __repr__(self):
+		return '<Listing %r>' % self.listing_id
+		
 
